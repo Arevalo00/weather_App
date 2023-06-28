ba774211e5c05428e52948fa3c9fc1b888a74b72
jennifer arevalo <arevalo5007@gmail.com>
2023-06-28 15:16:49 -0500

    HTML contaiers

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
- var weatherkey = api.openweathermap.org/data/2.5/forecast?id={cityID}&appid:'3150f18b814a4db9839615c732d90ed5';
+ // API call 
+ 
+ //var weatherkey = api.openweathermap.org/data/2.5/forecast?id={cityID}&appid:'3150f18b814a4db9839615c732d90ed5';
 
- console.log(weatherkey);
+
+// HTML elements calling to js 
+
+ var currentWeather = document.getElementsByClassName('currentWeather');
+ var button = document.getElementsByClassName('btn btn-primary');
+ var fiveDaycast = document.getElementsByClassName('fiveDaycast');
+
+
+addEventListener
